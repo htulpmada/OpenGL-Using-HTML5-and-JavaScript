ff713412adc354ac2b7df75d2b1322b2a94d59a9
adam <Apluth@crimson.ua.edu>
2017-01-20 16:03:41 -0600

    lets start back here

--- Project1/snowflake.js
+++ Project1/snowflake.js
@@ -2,15 +2,17 @@
 
 var canvas;
 var gl;
+var dir = 60;
 var start = vec2(-1,-1);
 var points = [];
 var vertices = [
     vec2( -1, -1 ),
     vec2(  0,  1 ),
-    vec2(  1, -1 )
+    vec2(  1, -1 ),
+    vec2( -1, -1 )
 ];
 
-var NumTimesToSubdivide = 3;
+var NumTimesToSubdivide = 1;
 
 window.onload = function init()
 {
@@ -26,9 +28,7 @@ window.onload = function init()
     // First, initialize the corners of our gasket with three points.
 
 
-    divideTriangle( 0.0, 1, NumTimesToSubdivide, vertices[0]);
-    divideTriangle( -120.0, 1, NumTimesToSubdivide, vertices[1]);
-    divideTriangle( 120.0, 1, NumTimesToSubdivide, vertices[2]);
+    divideTriangle( vertices, NumTimesToSubdivide);
 
     //
     //  Configure WebGL
@@ -56,47 +56,50 @@ window.onload = function init()
     render();
 };
 
-function divideTriangle( dir, len, count, prev )
-{
-    var mid = midP(a, b);
-    var slope = Slope(a, b);
-    return vec2(mid[0]-slope[1],mid[1]-slope[0]);
+function midP(a, b) { return vec2((a[0]+b[0])/2,(a[1]+b[1])/2);} 
 
+function Slope(a, b) { return vec2(b[0]-a[0],b[1]-a[1]);}
+function rotate_point(pointX, pointY, originX, originY, angle) {
+	angle = angle * Math.PI / 180.0;
+	return vec2(Math.cos(angle) * (pointX-originX) - Math.sin(angle) * (pointY-originY) + originX,
+		 Math.sin(angle) * (pointX-originX) + Math.cos(angle) * (pointY-originY) + originY);
+}
+function distance( a , b ){ return Math.sqrt(Math.pow((a[0]+b[0]),2)+(Math.pow((a[1]+b[1]),2)));}
+function getPoint( a, b )
+{
+    return rotate_point(b[0],b[1],a[0],a[1],60);
+    
+   // var d = distance(a,b);
+   // var angle = .0174533 * dir; 
+   // var cx = a[0] + d * Math.cos(angle); 
+   // var cy = a[1] + d * Math.sin(angle); 
+//    var mid = midP(a, b); 
+//    var slope = Slope(a, b); 
+//    slope[0]=-slope[0]; 
+    //return vec2(cx,cy); 
 }
 
 function divideTriangle( v, count )
 {
     while(count > 0){
-        for (var i = 0; i <= v.length - 1; i++){
+        for (var i = 0; i <= v.length - 2; i++){
             //bisect the sides
             var a = v[i];
-            if (i == v.length - 1) {
-                var b = v[0];
-            }
-            else {
+//            if (i === v.length - 1) {
+//                var b = v[0];
+//            }
+ //           else {
                 var b = v[i + 1];
-            }
+//            }
             var ab1 = mix(a, b, 1.0 / 3.0);
             var ab3 = mix(a, b, 2.0 / 3.0);
             points.push(a, ab1);
             var ab2 = getPoint( ab1 , ab3 );
             points.push(ab2, ab3, b);
         }
-        points.push(v[v.length - 1]);
+//        points.push(v[v.length - 1]);
         v = points;
         --count;
-    }
-    else {
-    	count--;
-    	//draw the four parts of the side _/\_ 
-    	var s = divideTriangle(dir, len/2, count, prev);
-	dir += 60.0;
-	s = divideTriangle(dir, len/2, count, s);
-	dir -= 120.0;
-	s = divideTriangle(dir, len/2, count, s);
-	dir += 60.0;
-	s = divideTriangle(dir, len/2, count, s);	
-        return s;
     }
 }
 
