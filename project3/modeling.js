639560ef1e5412fde51c33deef20a39b9a84e2bc
adam <Apluth@crimson.ua.edu>
2017-02-16 22:08:57 -0600

    for the record

--- project3/modeling.js
+++ project3/modeling.js
@@ -1,28 +1,22 @@
 "use strict";
 
 var gl;
+var thetaLoc;
 
-var nRows = 50;
-var nColumns = 50;
-
-// data for radial hat function: sin(Pi*r)/(Pi*r)
-
-var data = [];
-for( var i = 0; i < nRows; ++i ) {
-    data.push( [] );
-    var x = Math.PI*(4*i/nRows-2.0);
-
-    for( var j = 0; j < nColumns; ++j ) {
-        var y = Math.PI*(4*j/nRows-2.0);
-        var r = Math.sqrt(x*x+y*y);
-
-        // take care of 0/0 for r = 0
-
-        data[i][j] = r ? Math.sin(r) / r : 1.0;
-    }
-}
 
 var pointsArray = [];
+var topArr = [];
+var bottomArr = [];
+var handles = [];
+
+var numOfTris = 10;
+var maxNumTriangles = 200;
+var maxNumVertices = 3 * maxNumTriangles;
+
+var r = .5;
+
+var spin = false;
+var speed = 1;
 
 var fColor;
 
@@ -30,14 +24,21 @@ var near = -10;
 var far = 10;
 var radius = 6.0;
 var theta  = 0.0;
-var phi    = 0.0;
+var phi    = 5.0;
 var dr = 5.0 * Math.PI/180.0;
 
 const black = vec4(0.0, 0.0, 0.0, 1.0);
-const red = vec4(1.0, 0.0, 0.0, 1.0);
+const red = vec4( 1.0, 0.0, 0.0, 1.0 );  
+const yellow = vec4( 1.0, 1.0, 0.0, 1.0 );
+const green = vec4( 0.0, 1.0, 0.0, 1.0 );
+const blue = vec4( 0.0, 0.0, 1.0, 1.0 );
+const magenta = vec4( 1.0, 0.0, 1.0, 1.0 );
+const cyan = vec4( 0.0, 1.0, 1.0, 1.0 );
 
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
+
+var eye = vec3( 0,2,6);
 
 var left = -2.0;
 var right = 2.0;
@@ -69,14 +70,8 @@ window.onload = function init()
 // vertex array of nRows*nColumns quadrilaterals
 // (two triangles/quad) from data
 
-    for(var i=0; i<nRows-1; i++) {
-        for(var j=0; j<nColumns-1;j++) {
-            pointsArray.push( vec4(2*i/nRows-1, data[i][j], 2*j/nColumns-1, 1.0));
-            pointsArray.push( vec4(2*(i+1)/nRows-1, data[i+1][j], 2*j/nColumns-1, 1.0));
-            pointsArray.push( vec4(2*(i+1)/nRows-1, data[i+1][j+1], 2*(j+1)/nColumns-1, 1.0));
-            pointsArray.push( vec4(2*i/nRows-1, data[i][j+1], 2*(j+1)/nColumns-1, 1.0) );
-    }
-}
+    makePlayground();
+    
     //
     //  Load shaders and initialize attribute buffers
     //
@@ -86,7 +81,7 @@ window.onload = function init()
 
     var vBufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(topArr), gl.STATIC_DRAW);
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
@@ -99,18 +94,11 @@ window.onload = function init()
 
 // buttons for moving viewer and changing size
 
-    document.getElementById("Button1").onclick = function(){near  *= 1.1; far *= 1.1;};
-    document.getElementById("Button2").onclick = function(){near  *= 0.9; far *= 0.9;};
-    document.getElementById("Button3").onclick = function(){radius *= 2.0;};
-    document.getElementById("Button4").onclick = function(){radius *= 0.5;};
-    document.getElementById("Button5").onclick = function(){theta += dr;};
-    document.getElementById("Button6").onclick = function(){theta -= dr;};
-    document.getElementById("Button7").onclick = function(){phi += dr;};
-    document.getElementById("Button8").onclick = function(){phi -= dr;};
-    document.getElementById("Button9").onclick = function(){left  *= 0.9; right *= 0.9;};
-    document.getElementById("Button10").onclick = function(){left *= 1.1; right *= 1.1;};
-    document.getElementById("Button11").onclick = function(){ytop  *= 0.9; bottom *= 0.9;};
-    document.getElementById("Button12").onclick = function(){ytop *= 1.1; bottom *= 1.1;};
+    document.getElementById("start_stop").onclick = 
+            function(){
+                if(spin){spin = false;}
+                else{spin = true;}
+    };
 
     render();
 
@@ -121,26 +109,77 @@ function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var eye = vec3( radius*Math.sin(theta)*Math.cos(phi),
-                    radius*Math.sin(theta)*Math.sin(phi),
-                    radius*Math.cos(theta));
-
+    // spin playground y axis
+    theta = spin ? speed : 0;
+    //phi = spin ? phi + speed : phi;
+    var t = rotate(eye,at,theta);
+    eye = vec3(t[0],t[1],t[2]);
+    
     var modelViewMatrix = lookAt( eye, at, up );
     var projectionMatrix = ortho( left, right, bottom, ytop, near, far );
-
+    
+    
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
     // draw each quad as two filled red triangles
     // and then as two black line loops
 
-    for(var i=0; i<pointsArray.length; i+=4) {
+        // draw top
+//        gl.uniform3fv(thetaLoc, spinTheta);
+
         gl.uniform4fv(fColor, flatten(red));
-        gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
+        gl.drawArrays( gl.TRIANGLE_FAN, 0, numOfTris+1);
         gl.uniform4fv(fColor, flatten(black));
-        gl.drawArrays( gl.LINE_LOOP, i, 4 );
+        gl.drawArrays( gl.LINE_LOOP, 1, numOfTris);
+
+/*
+        // draw bottom
+        gl.uniform4fv(fColor, flatten(red));
+        gl.drawArrays( gl.TRIANGLE_FAN, (numOfTris+1)*2, numOfTris+1);
+        gl.uniform4fv(fColor, flatten(black));
+        gl.drawArrays( gl.LINE_LOOP, (numOfTris+1)*2, numOfTris+1);
+  
+        // draw handles
+        
+        gl.uniform4fv(fColor, flatten(red));
+        gl.drawArrays( gl.TRIANGLE_FAN, (numOfTris+1)*2, handles.length);
+        gl.uniform4fv(fColor, flatten(black));
+        gl.drawArrays( gl.LINE_LOOP, (numOfTris+1)*2, handles.length);
+  */      
+ 
+    requestAnimFrame(render);
+}
+
+
+function rotate(point, origin, angle) {
+    var pointX = point[0];
+    var pointY = point[1];
+    var originX = origin[0];
+    var originY = origin[1];
+    // Rotate point around origin given
+    angle = angle * Math.PI / 180.0;
+    return vec4(
+        Math.cos(angle) * (pointX-originX) - Math.sin(angle) * (pointY-originY) + originX,
+        Math.sin(angle) * (pointX-originX) + Math.cos(angle) * (pointY-originY) + originY,
+        point[2],
+        1.0);
+}
+
+function makePlayground(){
+    // account for last vertex of fan
+    numOfTris++;
+    //first point of triangle fan
+    var origin = new vec4(at[0],at[1],at[2],1.0);
+    topArr.push(origin);
+    
+    //first point of circle
+    var t = vec4(at[0],at[1],at[2],1.0);
+    t[0] += r;
+    
+    for(var i = 1; i <= numOfTris; i++){
+        topArr.push(t);
+        t = rotate(t,origin, 360/(numOfTris-1));
     }
 
-
-    requestAnimFrame(render);
 }
